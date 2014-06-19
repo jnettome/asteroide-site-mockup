a6a3f7dafe3125063740daee2a0223518acdd454
João Netto <hi@joaonetto.me>
2014-06-19 02:03:16 -0300

    Creating work page

---
+++ app/scripts/work.js
@@ -0,0 +1,35 @@
+'use strict';
+var _mainHeader = $('#header'),
+    waitForAnimationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+    verticalCenterAlignment;
+
+_mainHeader.addClass('animated fadeInDown').one(waitForAnimationEnd, function () {
+    $('#content').addClass('animated fadeInUp');
+    $('.page-header').addClass('animated fadeInDown');
+});
+
+verticalCenterAlignment = function(_element) {
+    var _height = _element.outerHeight();
+
+    _element.css('top', $(_element).parent().outerHeight() / 2).css('margin-top', (_height/2)*-1);
+    return _element;
+};
+
+$(window).on('resize load', function() {
+    $('.work-post .caption').css('width', $('.img-responsive').outerWidth());
+    $('.work-post .caption').css('height', $('.img-responsive').outerHeight());
+
+    verticalCenterAlignment($('.work-post .caption h3'));
+});
+
+$('.work-post a').on('mouseenter', function() {
+    $(this).find('.caption').removeClass('animated fadeOut').addClass('animated fadeIn').one(waitForAnimationEnd, function() {
+        // limpar classes
+    });
+});
+
+$('.work-post a').on('mouseleave', function() {
+    $(this).find('.caption').removeClass('animated fadeIn').addClass('animated fadeOut').one(waitForAnimationEnd, function() {
+        // limpar classes
+    });
+});
