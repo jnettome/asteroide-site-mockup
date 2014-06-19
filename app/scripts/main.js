243dc713895259d41582eac92dc773e2078107e8
João Netto <hi@joaonetto.me>
2014-06-18 23:23:20 -0300

    Initial animations on featured project

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,26 @@
 'use strict';
-console.log('\'Allo \'Allo!');
-$('#header').addClass('animated fadeInDown').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function animateMainText() {
-    $('.featured-background').addClass('animated fadeInDown');
+var _featuredBackground = $('.featured-background'),
+    _mainHeader = $('#header'),
+    _featuredText = $('h1.featured-text'),
+    _featuredPlay = $('.featured-play'),
+    waitForAnimationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+    verticalCenterAlignment;
+
+verticalCenterAlignment = function(_element) {
+    var _height = _element.outerHeight();
+
+    _element.css('top', $(window).outerHeight() / 2).css('margin-top', (_height/2)*1);
+    return _element;
+}
+
+verticalCenterAlignment(_featuredText);
+
+_mainHeader.addClass('animated fadeInDown').one(waitForAnimationEnd, function () {
+    _featuredBackground.backstretch(_featuredBackground.data('image-src'));
+    _featuredBackground.addClass('animated fadeInDown').one(waitForAnimationEnd, function() {
+    });
+
+    _featuredText.addClass('animated fadeInDown').one(waitForAnimationEnd, function() {
+        });
+    _featuredPlay.addClass('animated fadeInUp');
 });
